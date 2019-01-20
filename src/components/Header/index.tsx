1e38d980e27c2be99aabe46afeda9c87259b3efb
Youjingyu <735284268@qq.com>
2019-01-20 18:06:54 +0800

    feat: update Header
    header tag switch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,31 @@ import * as React from 'react';
 import './header.scss';
 
 export default class Header extends React.Component {
+  public state: { activeIndex: number}
+  constructor (props:any) {
+    super(props)
+    this.state = {
+      activeIndex: 0
+    }
+  }
   public render () {
+    const { activeIndex } = this.state
+    const tagName = ['广场', '好友', '自己']
     return (
       <nav className="header clearfix">
-        <span className="active">广场</span>
-        <span>好友</span>
-        <span>自己</span>
+        {tagName.map((name, i) => {
+          return (<span
+            key={i}
+            className={activeIndex === i ? 'active' : ''}
+            onClick={this.switch.bind(this, i)}
+            >{name}</span>)
+        })}
       </nav>
     );
+  }
+  private switch (index:number) {
+    this.setState({
+      activeIndex: index
+    })
   }
 }
